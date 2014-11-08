429c1e76f097b47e1ffe51c3c7b62b4a48cdf991
Christopher Reynoso <topherreynoso@gmail.com>
2014-11-08 10:24:58 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PlangradeClient::Application.config.secret_key_base = 'd284763fc6dbb4b2b8a5a2dd179a897fe666b1aa6b83ac884691e0b90230a4334bee9a1974da56431d1aa6940a40d26d8dfdb8162fc2d17759f46f077a895bbe'
