f38b19c8589901d8c5fb4f46ee24b19c16a42c3f
Christopher Reynoso <topherreynoso@gmail.com>
2014-11-08 19:53:12 -0700

    fixed redirect url

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
-  	@redirect_uri = "http://localhost:3000/redirect"
 	@client = OAuth2::Client.new(ENV['PLANGRADE_APP_ID'], ENV['PLANGRADE_SECRET'], :site => "https://plangrade.com")
   end
 
   def redirect
-  	redirect_uri = "http://localhost:3000/redirect"
   	client = OAuth2::Client.new(ENV['PLANGRADE_APP_ID'], ENV['PLANGRADE_SECRET'], :site => "https://plangrade.com")
-	token = client.auth_code.get_token(params[:code], :redirect_uri => redirect_uri)
+	token = client.auth_code.get_token(params[:code], :redirect_uri => redirect_url)
 	user_info = JSON.parse token.get('/api/v1/users/4').body
 	@user = user_info["user"]
 	companies_info = JSON.parse token.get('/api/v1/companies').body
